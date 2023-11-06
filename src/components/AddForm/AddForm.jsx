986a515754b7552383f140953267c045fdb7733e
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-11-06 18:43:21 +0100

    validation

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -1,23 +1,31 @@
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field } from 'formik';
+import { AddBtn, StyledForm } from './AddForm.styled';
+import * as Yup from 'yup';
+
+const Schema = Yup.object().shape({
+  name: Yup.string().min(2).required(),
+  number: Yup.number().min(7).required(),
+});
 
 export const AddForm = ({ onSubmit }) => {
   return (
     <Formik
       initialValues={{ name: '', number: '' }}
+      validation={Schema}
       onSubmit={(value, actions) => {
         onSubmit(value);
         actions.resetForm();
       }}
     >
-      <Form>
+      <StyledForm>
         <label htmlFor="name">Name</label>
         <Field name="name" />
 
         <label htmlFor="number">Number</label>
         <Field type="tel" name="number" />
 
-        <button type="submit">Add Contact</button>
-      </Form>
+        <AddBtn type="submit">Add Contact</AddBtn>
+      </StyledForm>
     </Formik>
   );
 };
